172c3781e84e0a74e6c7778357834437a82c0490
Cto8046 <caleboettle05@gmail.com>
2025-03-04 04:32:23 -0600

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 //This is a program that finds perfect numbers within a users given range
 
 
+import java.util.Scanner;
+
 public class Main {
 
     // Method to check for perfect numbers
@@ -15,6 +17,28 @@ public class Main {
                 if (num % i == 0) { // if it's a divisor
                     sum += i; // add it to sum
                 }
+            }
+
+            public static class UserInputting {
+            }
+            public void UserInputting(String[] args) {
+                Scanner scanner = new Scanner(System.in); // scanner obj to read input
+                long upperLimit = 0;
+
+                // Loop to keep asking until user enters a valid number
+                while (true) {
+                    System.out.print("Enter a number: ");
+
+                    if (scanner.hasNextLong()) { // check if input is a number
+                        upperLimit = scanner.nextLong(); // store number
+                        break; // exit loop
+                    } else {
+                        System.out.println("Nope, enter a number please."); // error msg
+                        scanner.next(); // clear bad input
+                    }
+                }
+
+                scanner.close();
             }
 
             // If sum of divisors = number, it's perfect
